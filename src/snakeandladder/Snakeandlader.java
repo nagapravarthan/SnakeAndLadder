3f37f96cbb0dbd381875afccb98eb0cce52ef019
nagapravarthan <nagapravarthannaidu8888@gmail.com>
2021-06-24 23:25:25 +0530

    snakeandladderuc6

--- src/snakeandladder/Snakeandlader.java
+++ src/snakeandladder/Snakeandlader.java
@@ -4,39 +4,42 @@ public class Snakeandlader
 {
 	public static void main(String[] args)
 	{
+				
+				int count = 0;
+			    int position = 0;
+			    while(position<=99){
+				System.out.println("position is " + position);
+				int die=(int) (Math.random() * 10) % 6+1;
+				position = (position + die);
+				count =(count + 1);
+				System.out.println("die value is " + die);
+				int value=(int) (Math.random() * 10) % 3+1;
+				
+				 switch(value) {
+				   case 1:
+				   System.out.println( "No Play You are in the same position!!!" );
+				   break;
+				  
+				   case 2:
+				   position = (position + die);
+				   if(position>100){
+					   position = (position - die);
+					   continue;
+				   }
+				   System.out.println( "your position after ladder is ::" + position );
+				   break;
+				   
+				   case 3: 
+				   position = (position - die);
+				  
+				  if(position<0){
+					  position = 0;
+				  }
+					 System.out.println( "Your Position after snake is::" + position );
+				   break;
+				   }
+				 }
+					System.out.println( "die play number:: " + count );
 
-		int position = 0;
-		  while(position<=99){
-			System.out.println("position is " + position);
-			int die=(int) (Math.random() * 10) % 6+1;
-			position =(position + die);
-			System.out.println("dice value is " + die);
-			int value=(int) (Math.random() * 10) % 3+1;
-			
-			 switch(value) {
-			   case 1:
-			   System.out.println( "No Play....You are in the same position" );
-			   break;
-			  
-			   case 2:
-			   position = (position + die);
-			   if(position>100){
-				  position = (position - die);
-				  continue;
-			   }
-			   System.out.println( "your position after ladder is :" + position );
-			   System.out.println("Roll again to reach 100");
-			   break;
-			   
-			   case 3: 
-			   position = (position - die);
-			  
-			  if(position<0){
-				  position = 0;
-			  }
-				 System.out.println( "Your Position after snake is" + position );
-			   break;
-			   }
-			 }
-	}
+		  }
 }
