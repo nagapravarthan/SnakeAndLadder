2fe6e6c12304e6b3e8bc2435986ec9486a2fae70
nagapravarthan <nagapravarthannaidu8888@gmail.com>
2021-06-24 22:36:44 +0530

    snakeandladderuc3

--- src/snakeandladder/Snakeandlader.java
+++ src/snakeandladder/Snakeandlader.java
@@ -5,14 +5,31 @@ public class Snakeandlader
 	public static void main(String[] args)
 	{
 
-		int Position=0;
-		System.out.println("Welcome to Snake-Ladder Program Problem");
-		System.out.println("You are at Starting Position::"+Position);
-	}
-		public static void Die()
-		{
-			int DieCheck = 1 +(int) Math.floor(Math.random()*6);
-			System.out.println("Dice Value::"+ DieCheck);
-		}
+		int position = 0;
+		System.out.println("position is " + position);
+		int die=(int) (Math.random() * 10) % 6+1;
+			System.out.println("dice value is " + die);
+		int value=(int) (Math.random() * 10) % 3+1;
+		System.out.println("case number is " + value);
+		 switch(value) {
+		   case 1:
+		   System.out.println( "No Play You are in the same position." );
+		   break;
+		  
+		   case 2:
+		   position = (position + die);
+		   System.out.println( "your position after ladder is ::" + position );
+		   break;
+		   
+		   case 3: 
+		   position = (position - die);
+		  
+		  if(position<0){
+			  position = 0;
+		  }
+			 System.out.println( "Your Position after snake is::" + position );
+		   break;
+		   }
+		 }
 
 }
