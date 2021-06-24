1ca33caba17be63a0051bc72704cbcbe46798b57
nagapravarthan <nagapravarthannaidu8888@gmail.com>
2021-06-24 22:32:00 +0530

    snakeandladderuc2

--- src/snakeandladder/Snakeandlader.java
+++ src/snakeandladder/Snakeandlader.java
@@ -2,13 +2,17 @@ package snakeandladder;
 
 public class Snakeandlader
 {
-
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
+
 		int Position=0;
 		System.out.println("Welcome to Snake-Ladder Program Problem");
 		System.out.println("You are at Starting Position::"+Position);
-		
 	}
+		public static void Die()
+		{
+			int DieCheck = 1 +(int) Math.floor(Math.random()*6);
+			System.out.println("Dice Value::"+ DieCheck);
+		}
 
 }
