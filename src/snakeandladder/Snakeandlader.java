b57a202beebcd37dddfe38d9b65576b05bef1f0d
nagapravarthan <nagapravarthannaidu8888@gmail.com>
2021-06-24 23:15:00 +0530

    snakeandladderuc4

--- src/snakeandladder/Snakeandlader.java
+++ src/snakeandladder/Snakeandlader.java
@@ -6,30 +6,36 @@ public class Snakeandlader
 	{
 
 		int position = 0;
-		System.out.println("position is " + position);
-		int die=(int) (Math.random() * 10) % 6+1;
-			System.out.println("dice value is " + die);
-		int value=(int) (Math.random() * 10) % 3+1;
-		System.out.println("case number is " + value);
-		 switch(value) {
-		   case 1:
-		   System.out.println( "No Play You are in the same position." );
-		   break;
-		  
-		   case 2:
-		   position = (position + die);
-		   System.out.println( "your position after ladder is ::" + position );
-		   break;
-		   
-		   case 3: 
-		   position = (position - die);
-		  
-		  if(position<0){
-			  position = 0;
+			  while(position<=93)
+			  {		
+				 int die= 1 + (int) (Math.random() *6);
+				 System.out.println("dice value is " + die);
+				 position =(position + die);
+				 System.out.println("position is " + position);
+				 int value= 1  + (int) (Math.random() * 3);
+			   switch(value) 
+			   {
+			   case 1:
+			   System.out.println( "No Play....You are in the same position" );
+			   break;
+			  
+			   case 2:
+			   position = (position + die);
+			  
+			   System.out.println( "your position after ladder is :" + position );
+			   break;
+			   
+			   case 3: 
+			   position = (position - die);
+			   System.out.println( "Your Position after snake is" + position );
+			  
+			  if(position<0)
+			  {
+				  position = 0;
+			  }
+			   break;
+			   }
 		  }
-			 System.out.println( "Your Position after snake is::" + position );
-		   break;
-		   }
-		 }
+	}
 
 }
