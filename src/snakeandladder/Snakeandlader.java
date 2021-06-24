4a3be3f3034149650b828a045f330ece0c0e5949
nagapravarthan <nagapravarthannaidu8888@gmail.com>
2021-06-24 22:21:42 +0530

    snakeandladder

---
+++ src/snakeandladder/Snakeandlader.java
@@ -0,0 +1,14 @@
+package snakeandladder;
+
+public class Snakeandlader
+{
+
+	public static void main(String[] args) 
+	{
+		int Position=0;
+		System.out.println("Welcome to Snake-Ladder Program Problem");
+		System.out.println("You are at Starting Position::"+Position);
+		
+	}
+
+}
